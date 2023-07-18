8e469ef6fa0fc9ba9d34ec7dda3b9c208604dc81
haizzdungnay <126995899+haizzdungnay@users.noreply.github.com>
2023-07-18 20:41:17 +0700

    Add files via upload

---
+++ ads.md
@@ -0,0 +1,10 @@
+﻿Viết chương trình chuyển đổi số nguyên N (cơ số 10) sang hệ cơ số B (2 <= B <= 32) bất kỳ.
+##### Yêu cầu:
+##### Dòng thứ 2 nhập số nguyên N 
+##### Dòng thứ 3 nhập cơ số B mà bạn muốn chuyển từ số nguyên N (cơ số 10)
+##### Ví dụ:
+##### input:
+>6
+>2
+##### output:
+>110
---
+++ solution.cpp
@@ -0,0 +1,35 @@
+﻿//chuyển đổi số nguyên N sang hệ cơ số B (2 <= B <= 32) bất kỳ.
+#include <iostream>
+#include <string>
+using namespace std;
+string convertToBase(int number, int base) {
+    string result = "";
+    while (number > 0)
+    {
+        int remainder = number % base;
+        char digit;
+
+        if (remainder < 10)
+        {
+            digit = '0' + remainder;
+        }
+        else
+        {
+            digit = 'A' + (remainder - 10);
+        }
+
+        result = digit + result;
+        number /= base;
+    }
+
+    return result;
+}
+int main() {
+    int number, base;
+    cin >> number;
+    cout << endl;
+    cin >> base;
+    string result = convertToBase(number, base);
+    cout << result << endl;
+    return 0;
+}
